d10da3eb21bfb91ebad508669654269a95e8bb34
st33v <github@f3rr3t.com>
2016-09-10 20:02:43 +1000

    replaced silly width=1200 with mode =5 (1296x730)

--- takepix.sh
+++ takepix.sh
@@ -4,14 +4,17 @@ picdir="/home/st33v/pix"
 picdate=$(date +%Y-%m-%d_%H%M)
 thispic=$picdir/$picdate.jpg
 
-echo thispic = $thispic
+#echo thispic = $thispic
 
-/opt/vc/bin/raspistill -x IFD1.Artist=neatherd -w 1200 -o $thispic
+/opt/vc/bin/raspistill -x IFD1.Artist=neatherd -q 70 -md 5 -o $thispic
+
+# silly portrait shape
+#/opt/vc/bin/raspistill -x IFD1.Artist=neatherd -w 1200 -o $thispic
 
 # now check to see if the pic is too dark (i.e. taken at night)
 mean=$(identify -format %[mean] $thispic | sed s/[.].*//)
 
-echo mean is "$mean"
+#echo mean is "$mean"
 
 # too dark
 if [ $mean -lt 2000 ]; then
