bc3e6aa0e192fc79b528a7fe51a97e178d7cfa44
st33v <github@f3rr3t.com>
2016-12-26 16:29:32 +1100

    Deleted all image processing (STAN does this now)

--- takepix.sh
+++ takepix.sh
@@ -1,36 +1,18 @@
 #!/bin/bash
 
 # define variables. Your needs may vary.
-# use literal user name. Shell expansions may not work when this script is run by a service!Y
+# use literal user name. Shell expansions may not work when this script is run by a service!
 picdir="/home/st33v/pix"   
 picdate=$(date +%Y-%m-%d_%H%M)
 thispic=$picdir/$picdate.jpg
 artist=$(hostname)
 
-
 # echo thispic = $thispic
 
 /opt/vc/bin/raspistill -x IFD1.Artist=${artist} -q 70 -md 4: -o $thispic
 
-# silly portrait shape
-#/opt/vc/bin/raspistill -x IFD1.Artist=neatherd -w 1200 -o $thispic
-
-# now check to see if the pic is too dark (i.e. taken at night)
-mean=$(identify -format %[mean] $thispic | sed s/[.].*//)
-
-# echo "mean is |$mean|"
-
-# too dark (nighttime)
-if [[ "${mean}" -lt 2000 ]] ; then
-    rm $thispic
-#    echo mean of $mean is too low.
-    exit 13
-fi
-
-# We can't resize image on this raspi because it has too little RAM!
-# Let's try it on STAN, which is a pi model 3B
-
-# convert ${thispic} -unsharp 1.5x1+0.7+0.02 $picdir/temp.jpg
-# cp ${thispic} $picdir/temp.jpg
-# convert $picdir/temp.jpg -resize 33% ${thispic}
-# rm $picdir/temp.jpg
+# Note: we do no image processing on this pi, because it has so little RAM
+# left after devoting 128MB to the GPU (camera).
+# STAN does all the heavy lifting:
+#   1) delete nightshots (where threshold pixel value is not met)
+#   2) resize and unsharp mask ready for display on the internet.
