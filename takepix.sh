26bd74123ad21ccc560e496b9f1e3e9eb3e4a1b9
st33v <github@f3rr3t.com>
2017-01-10 17:26:51 +1100

    generalised takepix.sh for various hosts and camera versions.

--- takepix.sh
+++ takepix.sh
@@ -6,10 +6,22 @@ picdir="/home/st33v/pix"
 picdate=$(date +%Y-%m-%d_%H%M)
 thispic=$picdir/$picdate.jpg
 artist=$(hostname)
+jpgQuality="90"
 
 # echo thispic = $thispic
 
-/opt/vc/bin/raspistill -x IFD1.Artist=${artist} -q 70 -md 4: -o $thispic
+case ${artist^^} in
+
+    LUCERNE)
+    /opt/vc/bin/raspistill -vf -hf -x IFD1.Artist=${artist} -q $jpgQuality -md 4: -o $thispic;;
+
+    NEATHERD)
+    /opt/vc/bin/raspistill -x IFD1.Artist=${artist} -q $jpgQuality -md 4: -o $thispic;;
+
+    *)
+    /opt/vc/bin/raspistill -x IFD1.Artist=${artist} -q $jpgQuality -md 4: -o $thispic;;
+
+esac
 
 # Note: we do no image processing on this pi, because it has so little RAM
 # left after devoting 128MB to the GPU (camera).
