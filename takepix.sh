0df9a91db4dd190a47371eba53556713214d1f43
st33v <github@f3rr3t.com>
2017-01-14 15:26:57 +1100

    now PUSHES pix to stan rather than waiting for them to be collected

--- takepix.sh
+++ takepix.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-
-# define variables. Your needs may vary.
-# use literal user name. Shell expansions may not work when this script is run by a service!
-picdir="/home/st33v/pix"   
+# takepix.sh  SJP 10 January 2017
+# Version 2: PUSH photos to STAN rather than waiting for STAN to pull them by scp.
+#
+# Create a file called 'picdir.config', containing paths to your local photos and 
+# the storage location on your equivalent of STAN, or 'cam Herder' raspi. It must overwrite
+# the following variable assignments, which are shown as examples:
+#   use literal user name. Shell expansions may not work when this script is run by a service!
+picdir="/path/to/my/photos"  # overwritten by sourcing from .picdir.config
+camHerder="STAN:/home/path/to/stored/photos"
+. picdir.config             # source directory name from local config file
 picdate=$(date +%Y-%m-%d_%H%M)
 thispic=$picdir/$picdate.jpg
 artist=$(hostname)
 jpgQuality="90"
+camHerderHostname='stan'    # Substitute your cam 'controller' hostname or LAN IP address
 
 # echo thispic = $thispic
 
@@ -22,6 +29,10 @@ case ${artist^^} in
     /opt/vc/bin/raspistill -x IFD1.Artist=${artist} -q $jpgQuality -md 4: -o $thispic;;
 
 esac
+
+# Copy pic to STAN, and place it in proper subdirectory (which of course must exist on STAN).
+scp $thispic $camHerder/$artist/.
+rm $thispic
 
 # Note: we do no image processing on this pi, because it has so little RAM
 # left after devoting 128MB to the GPU (camera).
